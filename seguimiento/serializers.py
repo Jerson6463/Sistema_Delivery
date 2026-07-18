e2fe556ceab412ae57fa77d1a930c9ba66120c02
Jerson6463 <mreyesjersoneli@uss.edu.pe>
2026-07-18 01:38:45 -0500

    Delete seguimiento directory

--- seguimiento/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import SeguimientoEntrega
-
-class SeguimientoEntregaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SeguimientoEntrega
-        fields = ['id', 'pedido', 'latitud', 'longitud', 'timestamp']
-        read_only_fields = ['id', 'pedido', 'timestamp']
--- seguimiento/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ActualizarUbicacionView, ObtenerUltimaUbicacionView
-
-urlpatterns = [
-    path('pedidos/<int:pedido_id>/actualizar/', ActualizarUbicacionView.as_view(), name='actualizar_ubicacion'),
-    path('pedidos/<int:pedido_id>/rastrear/', ObtenerUltimaUbicacionView.as_view(), name='rastrear_pedido'),
-]
